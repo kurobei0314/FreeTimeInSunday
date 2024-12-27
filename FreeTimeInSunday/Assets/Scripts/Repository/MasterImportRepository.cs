ce74ddc44930b616fba2e521fba7156bad55d347
kurobei0314 <maikurodei.biseibutu@gmail.com>
2024-12-27 09:04:54 +0900

    [add] addressable関係を設定(エラー出てる)

--- FreeTimeInSunday/Assets/Scripts/Repository/MasterImportRepository.cs
+++ FreeTimeInSunday/Assets/Scripts/Repository/MasterImportRepository.cs
@@ -1,9 +1,16 @@
-using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.AddressableAssets;
+using Cysharp.Threading.Tasks;
+using System;
+using System.Linq;
 
 public class MasterImportRepository
 {
-    public void Initialize()
+    public async UniTask LoadEventDTO(Action<List<EventDTO>> callback)
     {
-      
+      var dataStore = Addressables.LoadAssetAsync<EventVODataStore>("Assets/Master/EventVODataStore");
+      await dataStore;
+      var eventDTOs = dataStore.Result.Select(vo => new EventDTO(vo)).ToList();
+      callback?.Invoke(eventDTOs);
     }
 }
