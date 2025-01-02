c71643f4ebfa9b70a37abde58510826bf5ff19e0
kurobei0314 <maikurodei.biseibutu@gmail.com>
2025-01-02 13:48:25 +0900

    [add] HPが減る仕組みをview側に作成

--- FreeTimeInSunday/Assets/Scripts/View/PlayerHPView.cs
+++ FreeTimeInSunday/Assets/Scripts/View/PlayerHPView.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHPView : MonoBehaviour
 {
-    [SerializeField] private Transform[] _hpIconArray;
+    [SerializeField] private Image[] _hpIconArray;
+    [SerializeField] private Sprite _hpImage;
+    [SerializeField] private Sprite _noHpImage;
     
     public void Initialize(int hp)
     {
-        
+        for (var i = 0; i < _hpIconArray.Length; i++)
+        {
+            _hpIconArray[i].sprite =  (i < hp) ? _hpImage : _noHpImage;
+        }
     }
 }
